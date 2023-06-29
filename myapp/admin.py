cae09103f880db13928534bef6a3d161ae891ac4
Sh1mpi <meqalion2@gmail.com>
2023-06-29 16:06:18 +0300

    fix: admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from myapp.models import InGreenhouse,Compatibility,Plant,Temperature,PlantCare
+from myapp.models import Greenhouse, InGreenhouse, Compatibility, Plant, Temperature, PlantCare
 
-# Register your models here.
-admin.site.register(InGreenhouse)
-admin.site.register(Compatibility)
-admin.site.register(Plant)
-admin.site.register(Temperature)
-admin.site.register(PlantCare)
+class GreenhouseAdmin(admin.ModelAdmin):
+    list_display = ['user', 'rows', 'row_length']
+
+class InGreenhouseAdmin(admin.ModelAdmin):
+    list_display = ['user', 'plant', 'cell', 'planting_date']
+    date_hierarchy = 'planting_date'
+
+class CompatibilityAdmin(admin.ModelAdmin):
+    list_display = ['plant']
+    filter_horizontal = ['compatible_with', 'incompatible_with']
+
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'time', 'image']
+
+class TemperatureAdmin(admin.ModelAdmin):
+    list_display = ['temperature', 'time']
+
+class PlantCareAdmin(admin.ModelAdmin):
+    list_display = ['plant', 'care_instructions']
+
+admin.site.register(Greenhouse, GreenhouseAdmin)
+admin.site.register(InGreenhouse, InGreenhouseAdmin)
+admin.site.register(Compatibility, CompatibilityAdmin)
+admin.site.register(Plant, PlantAdmin)
+admin.site.register(Temperature, TemperatureAdmin)
+admin.site.register(PlantCare, PlantCareAdmin)
--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from myapp.models import Greenhouse, Plant, InGreenhouse, Compatibility, Temperature, PlantCare
 
-# Create your tests here.
+class GreenhouseModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(username='testuser', password='12345')
+        test_user = User.objects.get(username='testuser')
+        Greenhouse.objects.create(user=test_user, rows=5, row_length=10)
+
+    def test_greenhouse_content(self):
+        greenhouse = Greenhouse.objects.get(id=1)
+        expected_user = f'{greenhouse.user}'
+        expected_rows = f'{greenhouse.rows}'
+        expected_row_length = f'{greenhouse.row_length}'
+        self.assertEqual(expected_user, 'testuser')
+        self.assertEqual(expected_rows, '5')
+        self.assertEqual(expected_row_length, '10.0')
+
